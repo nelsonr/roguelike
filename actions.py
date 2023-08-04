01121fd4e958651d21e74a21529f31818464ac13
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2023-08-04 21:17:29 +0100

    Add Engine, Game Map and Entities

--- actions.py
+++ actions.py
@@ -1,14 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """
+        Perform this action with the objects needed to determine its scope.
+
+        `engine` is the scope where the action is being peformed in.
+
+        `entity` is the object performing the action.
+
+        This method must be overriden by Action subclasses.
+        """
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
-        super().__init__
+        super().__init__()
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination out of bounds.
+
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+
+        entity.move(self.dx, self.dy)
