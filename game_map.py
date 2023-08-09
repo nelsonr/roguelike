fa9b5217e70164835a23feab5fb5a80d340ff8b0
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2023-08-09 23:23:31 +0100

    Add field of view

--- game_map.py
+++ game_map.py
@@ -7,9 +7,24 @@ import tile_types
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
+
         self.tiles = np.full(
             (width, height),
             fill_value=tile_types.wall,
+            order="F"
+        )
+
+        # Tiles the player can currently see
+        self.visible = np.full(
+            (width, height),
+            fill_value=False,
+            order="F"
+        )
+
+        # Tiles the player has seen before
+        self.explored = np.full(
+            (width, height),
+            fill_value=False,
             order="F"
         )
 
@@ -18,4 +33,15 @@ class GameMap:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Renders the map.
+
+        If a tile is in the "visible" array, then draw it with the "light" colors.
+        If it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
+        Otherwise, the default is "SHROUD".
+        """
+        console.rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
